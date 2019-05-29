37281aa2fdce8ef91bf0d08aefb26b8240f4994d
huietj1 <huietj1@nku.edu>
2019-05-28 21:11:37 -0400

    switched to sqlite

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import models, { sequelize } from './models';
+import models, {
+    sequelize
+} from './models';
+import routes from './routes';
 
 const app = express();
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(async (req, res, next) => {
+    req.context = {
+        models,
+    };
+    next();
+});
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+
+
 
 var whitelist = ['http://example1.com', 'http://example2.com'];
 
@@ -28,10 +48,12 @@ var corsOptions = {
 
 
 app.get('/products/:id', cors(corsOptions), function (req, res, next) {
-    res.json({ msg: 'This is CORS-enabled for only example.com.' })
+    res.json({
+        msg: 'This is CORS-enabled for only example.com.'
+    })
 });
 //todo: look into implementing migration instead of synch.
-//find out how to create api calls for crud (other app has info)
+
 sequelize.sync().then(() => {
     app.listen(process.env.PORT, () => {
         console.log(`Example app listening on port ${process.env.PORT}!`);
--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,21 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.DATABASE_USERNAME,
-    process.env.DATABASE_PASSWORD,
-    {
-        dialect: 'postgres',
-    },
-);
+// const sequelize = new Sequelize(
+//     process.env.DATABASE,
+//     process.env.DATABASE_USERNAME,
+//     process.env.DATABASE_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//     },
+// );
 
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './testDb'
+  });
+
+
+  
 const models = {
     Activity_Attendance: sequelize.import('./activity_attendance'),
     User: sequelize.import('./user'),
