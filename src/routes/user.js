e7f580139305edb971fb972e77f8ce62150d5f2f
jacob <huietj1@nku.edu>
2019-06-04 16:34:43 -0400

    validation POC and debugging implemented

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,22 @@
 import { Router } from 'express';
+import validator from 'validator';
 const status = require('http-status');
 const router = Router();
 
-router.post('/', async (req, res) => {
-  const user = await req.context.models.User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-    // points: req.body.points,
-    // phoneNumbur: req.body.phoneNumber
-  });
-  return res.status(status.CREATED).json(user);
+router.post('/', (req, res) => {
+  return validateUserPost(req)
+  .then(validated => {
+    if(validated) return Promise.reject(validated);
+    return req.context.models.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    })
+  }).then(user => {
+    return res.status(status.CREATED).json(user);
+  }).catch(validError => res.status(status.BAD_REQUEST).json(validError));
+//todo: validate password
 });
 
 
@@ -26,6 +31,7 @@ router.get('/:userId', async (req, res) => {
 });
 
 router.put('/:userId', async (req, res, next) => {
+
   const user = await req.context.models.User.findByPk(req.params.userId);
   if (!user) {
     throw new Error('user not found in database');
@@ -37,7 +43,7 @@ router.put('/:userId', async (req, res, next) => {
   user.password = req.body.password;
   user.phoneNumbur = req.body.phoneNumber;
   user.role = req.body.role;
-  
+
   const updatedUser = await user.save();
   // await user.save().catch();
   res.status(status.OK).json(updatedUser);
@@ -53,7 +59,21 @@ router.delete('/:userId', async (req, res, next) => {
   return res.status(status.OK).json(user);
 });
 
+function validateUserPost( req ){
+  return Promise.resolve(syncValidateUserPost(req));
+}
 
+function syncValidateUserPost( req ) {
+  if( !req.body.password ) return "Invalid password entered";
+  if( !req.body.email || !validator.isEmail(req.body.email) ) return "invalid email entered";
+  if( !req.body.firstName || !validator.isAlpha(req.body.firstName) ) return "invalid first name entered";
+  if( !req.body.lastName || !validator.isAlpha(req.body.lastName) ) return "invalid last name entered";
+  return req.context.models.User.findOne({ where: { email: req.body.email } })
+    .then(model => {
+      if(model) return "This email is already in use";
+      return "";
+    });
+}
 
 // router.get('/:userId/points', async (req, res) => {
 //   const user = await req.context.models.User.findByPk(req.params.userId);
